4846c947137e355121ff8731ee4f1dc42ccbe13f
canele452 <canele452@gmail.com>
2024-01-31 22:14:33 +0900

    html 받음

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+import requests
+from bs4 import BeautifulSoup
 import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get("https://leagueoflegends.fandom.com/wiki/League_of_Legends_Wiki")
+url="https://leagueoflegends.fandom.com/wiki/League_of_Legends_Wiki"
+
+response = requests.get(url)
+soup = BeautifulSoup(response.content,'html.parser')
+print(soup.find_all("span",class_="grid-icon"))
