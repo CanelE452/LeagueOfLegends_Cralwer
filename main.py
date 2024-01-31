42a6252eddb7d2af5f4a88d1132e89a84759b491
canele452 <canele452@gmail.com>
2024-01-31 21:38:23 +0900

    basic setting

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-dasf
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("https://leagueoflegends.fandom.com/wiki/League_of_Legends_Wiki")
